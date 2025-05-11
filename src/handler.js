876642b051d0989faddccd1db57d7b3b3a1fb548
unknown <harahmanabdarib@gmail.com>
2025-05-12 00:17:10 +0800

    feat: add handler to get book by ID

--- src/handler.js
+++ src/handler.js
@@ -69,4 +69,28 @@ const getAllBooksHandler = (request, h) => {
   return response;
 };
 
-module.exports = { addBookHandler, getAllBooksHandler };
+const getBookByIdHandler = (request, h) => {
+  const { id } = request.params;
+
+  const book = books.find((n) => n.id === id);
+
+  if (book != undefined){
+    const response = h.response({
+      status: 'success',
+      data: {
+        book,
+      },
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
+
+module.exports = { addBookHandler, getAllBooksHandler, getBookByIdHandler };
